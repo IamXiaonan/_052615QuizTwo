d0a4e923063de01ddcac7d944c66833a612f15a5
MissWan <alinawxn@gmail.com>
2015-05-26 13:26:59 -0400

    Initial Commit

---
+++ _052615QuizTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  _052615QuizTwo
+//
+//  Created by Xiaonan Wang on 5/26/15.
+//  Copyright (c) 2015 Xiaonan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
